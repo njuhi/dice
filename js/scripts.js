8939af4359694d5a2d01fa051cffb190ef73a42c
njuhi <morphynjuh@mail.com>
2019-03-06 15:47:47 +0300

    save changes

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+var score = [];
+var totalScore = [];
+var currentScore = 0;
+ function roll(){
+    var randomNumber=parseInt(Math.floor(Math.random() * 6 + 1))
+   document.getElementById("player1").innerHTML = randomNumber;
+    if(randomNumber!=1){
+      currentScore+=randomNumber;
+    }
+    else{
+      currentScore=0;
+      score=0;
+    }
+};
+function hold(){
+
+  score.push(currentScore);
+  alert(score);
+  alert("hi");
+  // score.forEach(function(sum){
+  //   totalScore+=sum;
+  // });
+
+  totalScore.push(score);
+  alert(totalScore);
+
+  for(var i=0; i<=score.length; i++){
+    totalScore+=score[i];
+  }
+
+  alert(totalScore);
+}
+
+
+var sco = [];
+var totSco = [];
+var currSco = 0;
+ function llor(){
+    var randomNumber=parseInt(Math.floor(Math.random() * 6 + 1))
+   document.getElementById("player2").innerHTML = randomNumber;
+    if(randomNumber!=1){
+      currSco+=randomNumber;
+    }
+    else{
+      currSco=0;
+      sco=0;
+    }
+};
+function dloh(){
+
+  sco.push(currSco);
+  alert(sco);
+  alert("hi");
+  // score.forEach(function(sum){
+  //   totalScore+=sum;
+  // });
+
+  totSco.push(sco);
+  alert(totSco);
+
+  for(var i=0; i<=sco.length; i++){
+    totSco+=sco[i];
+  }
+
+  alert(totSco);
+}
